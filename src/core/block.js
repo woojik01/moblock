dc3b961833534b14b1478aacbe46c03e4e35289a
woojik01 <woojik1220@gmail.com>
2026-09-01 08:44:32 +0900

    fix: restore block chain append helper

--- src/core/block.js
+++ src/core/block.js
@@ -8,6 +8,14 @@ export function createBlock(type, fields = {}, inputs = {}, next = null) {
   };
 }
 
+export function appendBlock(head, block) {
+  if (!head) return block;
+  let current = head;
+  while (current.next) current = current.next;
+  current.next = block;
+  return head;
+}
+
 export function cloneBlock(block) {
   return structuredClone(block);
 }
