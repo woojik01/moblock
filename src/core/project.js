30b7de14ea6e9786d1f760add3baee9fe6f737b5
woojik01 <woojik1220@gmail.com>
2026-08-31 19:34:25 +0900

    refactor: establish project scene object model

--- src/core/project.js
+++ src/core/project.js
@@ -1,18 +1,29 @@
-export function createProject(name = 'Untitled Game') {
+import { createGameObject } from './game-object.js';
+
+export function createScene(name = 'Main Scene') {
   return {
-    version: 1,
+    id: crypto.randomUUID(),
+    name,
+    objects: [],
+    scripts: []
+  };
+}
+
+export function createProject(name = 'Untitled Game') {
+  const mainScene = createScene('Main Scene');
+  mainScene.id = 'main';
+  mainScene.objects.push(createGameObject('Player'));
+
+  return {
+    version: 2,
     name,
     settings: { width: 640, height: 360, fps: 60 },
     assets: [],
     variables: [],
-    scenes: [
-      {
-        id: 'main',
-        name: 'Main Scene',
-        objects: [],
-        scripts: []
-      }
-    ],
+    lists: [],
+    messages: [],
+    functions: [],
+    scenes: [mainScene],
     get scene() {
       return this.scenes[0];
     }
